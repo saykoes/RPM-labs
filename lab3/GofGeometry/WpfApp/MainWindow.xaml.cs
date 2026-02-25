5e6fd884706d6c17ac7238d0729130a18500e5ab
Zerwana.Kukurydza <50077514+ZerwanaKukurydza@users.noreply.github.com>
2026-02-26 00:02:16 +0700

    Lab3: Start

---
+++ lab3/GofGeometry/WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+
+    public abstract class Figure
+    {
+        public Color Color { get; set; }
+        // Метод, создающий визуальный элемент для отображения
+        public abstract UIElement CreateUIElement(double size = 50);
+    }
+
+    public class Circle : Figure
+    {
+        public override UIElement CreateUIElement(double size = 50)
+        {
+            return new Ellipse
+            {
+                Width = size,
+                Height = size,
+                Fill = new SolidColorBrush(Color),
+                Margin = new Thickness(5)
+            };
+        }
+    }
+    public class Square : Figure
+    {
+        public override UIElement CreateUIElement(double size = 50)
+        {
+            return new Rectangle
+            {
+                Width = size,
+                Height = size,
+                Fill = new SolidColorBrush(Color),
+                Margin = new Thickness(5)
+            };
+        }
+    }
+    public class Triangle : Figure
+    {
+        public override UIElement CreateUIElement(double size = 50)
+        {
+            var polygon = new Polygon
+            {
+                Points = new PointCollection
+                {
+                    new Point(size / 2, 0),
+                    new Point(0, size),
+                    new Point(size, size)
+                },
+                Fill = new SolidColorBrush(Color),
+                Margin = new Thickness(5),
+                Width = size,
+                Height = size,
+                Stretch = Stretch.Fill
+            };
+            return polygon;
+        }
+    }
+
+    public abstract class CircleCreator
+    {
+        public abstract Circle CreateCircle();
+    }
+
+    public class RedCircleCreator : CircleCreator
+    {
+        public override Circle CreateCircle() => new Circle
+        {
+            Color = Colors.Red
+        };
+    }
+    ///Аналогично для других цветов
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
