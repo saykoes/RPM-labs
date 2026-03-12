ccb606d3695a7e138c571431d3626e6b1216c533
Сайко Егор Сергеевич <SaykoES@dom201.local>
2026-03-12 10:52:47 +0700

    Lab5: Step 1

---
+++ lab5/GofCAF/GofCAF/Models/FileSystem.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GofCAF.Models
+{
+    public abstract class FileSystemItem
+    {
+        public string Name { get; protected set; }
+
+        public FileSystemItem(string name)
+        {
+            Name = name;
+        }
+
+        public abstract long Size { get; }
+        public abstract void Add(FileSystemItem item);
+
+        public abstract void Remove(FileSystemItem item);
+        public abstract FileSystemItem? GetChild(int index);
+    }
+
+    public class File : FileSystemItem
+    {
+        protected long _size;
+        public override long Size => _size;
+
+        public File(long size, string name) : base(name)
+        {
+            _size = size;
+        }
+
+        public override void Add(FileSystemItem item)
+        {
+            throw new InvalidOperationException("File cannot have children");
+        }
+
+        public override void Remove(FileSystemItem item)
+        {
+            throw new InvalidOperationException("File cannot have children");
+        }
+
+        public override FileSystemItem? GetChild(int index)
+        {
+            throw new InvalidOperationException("File cannot have children");
+        }
+    }
+
+    public class Folder : FileSystemItem
+    {
+        protected readonly List<FileSystemItem> Children;
+
+        public Folder(string name) : base(name)
+        {
+            this.Children = new List<FileSystemItem>();
+        }
+
+        public override long Size => Children.Sum(c => c.Size);
+
+        public override void Add(FileSystemItem item)
+        {
+            if (Children.Any(i => i.Name == item.Name))
+                throw new InvalidOperationException("Item with this name already exists");
+
+            Children.Add(item);
+        }
+
+        public override void Remove(FileSystemItem item)
+        {
+            if (!Children.Contains(item))
+                throw new InvalidOperationException("File not found");
+            
+            Children.Remove(item);
+        }
+
+        public override FileSystemItem? GetChild(int index) =>
+            Children.ElementAtOrDefault(index);
+    }
+
+
+}
---
+++ lab5/GofCAF/GofCAF/Program.cs
@@ -0,0 +1,41 @@
+﻿using GofCAF.Models;
+using System.Reflection;
+using GofFile = GofCAF.Models.File;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        void TryExecute(Action action)
+        {
+            try
+            {
+                action();
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine($"ERROR: {ex.Message}");
+            }
+        }
+
+        Folder yabaiFolder = new Folder("yabai");
+
+        GofFile yabaiFile = new GofFile(2000, "eeee");
+        yabaiFolder.Add(yabaiFile);
+        yabaiFolder.Add(new GofFile(1000, "unn"));
+
+        Folder myFolder = new Folder("my");
+        yabaiFolder.Add(myFolder);
+
+        myFolder.Add(new GofFile(500, "test"));
+
+        Console.WriteLine(yabaiFolder.Size);
+
+        TryExecute(() => yabaiFolder.Add(new Folder("my")));
+        TryExecute(() => myFolder.Add(new GofFile(500, "test")));
+        TryExecute(() => yabaiFile.Add(new GofFile(500, "test")));
+
+        TryExecute(() => myFolder.Remove(myFolder.GetChild(0)));
+        TryExecute(() => myFolder.Remove(myFolder.GetChild(0)));
+    }
+}
