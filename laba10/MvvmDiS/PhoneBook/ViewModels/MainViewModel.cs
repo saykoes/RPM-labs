872e5f98843f2c172640a81e6c3cccd697fea1bb
Zerwana.Kukurydza <50077514+ZerwanaKukurydza@users.noreply.github.com>
2026-04-26 18:05:26 +0700

    Lab 10: Init

---
+++ laba10/MvvmDiS/PhoneBook/ViewModels/MainViewModel.cs
@@ -0,0 +1,72 @@
+﻿using PhoneBook.Core;
+using PhoneBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PhoneBook.ViewModels
+{
+    public class MainViewModel : ObservableObject
+    {
+        public ObservableCollection<Contact> Contacts { get; }
+
+        private string _name = string.Empty;
+        private string _phone = string.Empty;
+        private Contact? _selectedContact;
+        private int id = 0;
+
+        public string Name
+        {
+            get => _name;
+            set => Set(ref _name, value);
+        }
+        public string Phone
+        {
+            get => _phone;
+            set => Set(ref _phone, value);
+        }
+        public Contact? SelectedContact
+        {
+            get => _selectedContact;
+            set => Set(ref _selectedContact, value);
+        }
+
+        public ICommand AddCommand { get; }
+        public ICommand DeleteCommand { get; }
+
+        public MainViewModel()
+        {
+            Contacts = new ObservableCollection<Contact>();
+            AddCommand = new RelayCommand(
+            AddContact,
+            CanAddContact);
+
+            DeleteCommand = new RelayCommand(
+            DeleteContact,
+            CanDeleteContact);
+        }
+
+        private void AddContact()
+        {
+            Contact c = new Contact(id++, Name, Phone);
+            if (c.Validate())
+            {
+                Contacts.Add(c);
+                Name = string.Empty;
+                Phone = string.Empty;
+            }
+        }
+        private bool CanAddContact() => !string.IsNullOrEmpty(Name) && !string.IsNullOrEmpty(Phone) && Contact.IsPhoneValid(Phone);
+
+        private void DeleteContact()
+        {
+            if (SelectedContact is not null && Contacts.Contains(SelectedContact))
+                Contacts.Remove(SelectedContact);
+        }
+        private bool CanDeleteContact() => SelectedContact is not null;
+    }
+}
---
+++ laba10/MvvmDiS/readme.md
@@ -0,0 +1,23 @@
+## Lab 10. MVVM: Dependency Injection, Service Pattern
+## Dependency Injection и паттерн «Сервис» в MVVM-приложениях
+### Цель работы: 
+изучить принцип внедрения зависимостей (Dependency Injection, DI) и паттерн «Сервис» для решения проблемы жёсткой связности компонентов в MVVM-приложениях.
+
+### Задание:
+Выполнить рефакторинг приложения «Телефонная книга» из Лабораторной работы №9 с применением паттернов Dependency Injection и Service.
+
+--- 
+
+### Theory
+
+
+### Practice
+
+```csharp
+
+```
+
+### In MainWindow.xaml
+
+### Summary
+I've successfully utilized Dependecy Injection and implemented Service pattern
