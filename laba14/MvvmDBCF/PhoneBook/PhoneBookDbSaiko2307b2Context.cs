9983c060a3f02d879d6d5f02d388456bc5a9505a
Zerwana.Kukurydza <50077514+ZerwanaKukurydza@users.noreply.github.com>
2026-06-04 11:43:02 +0700

    Lab14: Init

---
+++ laba14/MvvmDBCF/PhoneBook/PhoneBookDbSaiko2307b2Context.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using PhoneBook.Models;
+namespace PhoneBook;
+
+public partial class PhoneBookDbSaiko2307b2Context : DbContext
+{
+    public PhoneBookDbSaiko2307b2Context()
+    {
+    }
+
+    public PhoneBookDbSaiko2307b2Context(DbContextOptions<PhoneBookDbSaiko2307b2Context> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Contact> Contacts { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Contact>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Contacts__3214EC07CAB4834C");
+
+            entity.Property(e => e.Name).HasMaxLength(100);
+            entity.Property(e => e.Phone).HasMaxLength(20);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ laba14/MvvmDBCF/PhoneBook/ViewModels/AboutViewModel.cs
@@ -0,0 +1,18 @@
+﻿using PhoneBook.Core;
+using PhoneBook.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PhoneBook.ViewModels
+{
+    public class AboutViewModel : ViewModelBase
+    {
+        public string AppName => "PhoneBook";
+        public string Version => "v3.0 (Nav)";
+        public AboutViewModel(INavigationService nav) : base(nav) {}
+    }
+}
